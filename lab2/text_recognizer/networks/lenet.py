0e5a5e41fc92ea4c92272d021e8ee2d7018dfa85
carlb15 <carlb15@local>
2018-08-03 23:59:39 +0000

    my lab3 work

--- lab2/text_recognizer/networks/lenet.py
+++ lab2/text_recognizer/networks/lenet.py
@@ -9,8 +9,31 @@ def lenet(input_shape: Tuple[int, ...], output_shape: Tuple[int, ...]) -> Model:
     num_classes = output_shape[0]
 
     ##### Your code below (Lab 2)
-
+    model = Sequential()
+    activation = 'relu'
+    inputShape = input_shape
+    
+    if len(input_shape) < 3:
+        model.add(Lambda(lambda x: tf.expand_dims(x, -1), input_shape=input_shape))
+        inputShape = (input_shape[0], input_shape[1], 1)
+    
+    # 1st conv -> activation -> pool layers
+    model.add(Conv2D(20, 5, padding='same', activation='relu', input_shape=inputShape))
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(2, 2)))
+    
+    # 2nd conv -> activation -> pool layers
+    model.add(Conv2D(50, 5, padding='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+    model.add(Dropout(0.2))
+    
+    # First FC -> Activation
+    model.add(Flatten())
+    model.add(Dense(500, activation='relu'))
+    model.add(Dropout(0.2))
+    
+    # Second FC layer
+    model.add(Dense(num_classes, activation='softmax'))
+    
     ##### Your code above (Lab 2)
 
     return model
-
